304f1f6a4dc8883c61e23eace8d7edb975fcc34a
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2024-09-21 22:26:01 +0300

    Update Program.cs

--- CinemaApp/CSharpWeb.CinemaApp.Sept2024/CinemaApp.Web/Program.cs
+++ CinemaApp/CSharpWeb.CinemaApp.Sept2024/CinemaApp.Web/Program.cs
@@ -15,7 +15,7 @@ namespace CinemaApp.Web
             if (!app.Environment.IsDevelopment())
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                // this is forbidden for production, because it return what type is the error and client should not be able to see that  
                 app.UseHsts();
             }
 
