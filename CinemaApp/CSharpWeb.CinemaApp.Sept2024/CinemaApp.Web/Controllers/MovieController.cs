4570468b7d9d2336eb6f89b3e4b8f9821ed7859a
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2024-09-23 16:43:14 +0300

    update

---
+++ CinemaApp/CSharpWeb.CinemaApp.Sept2024/CinemaApp.Web/Controllers/MovieController.cs
@@ -0,0 +1,43 @@
+﻿using CinemaApp.Data;
+using CinemaApp.Data.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CinemaApp.Web.Controllers
+{
+    public class MovieController : Controller
+    {
+        private readonly CinemaDbContext dbContext;
+
+        // dependency injection of our DbContext
+        public MovieController(CinemaDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            IEnumerable<Movie> allMovies = this.dbContext
+                .Movies
+                .ToList();
+
+            return View(allMovies);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return this.View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Movie movie)
+        {
+            //TODO: Add form model + validation
+            this.dbContext.Movies.Add(movie);
+            this.dbContext.SaveChanges();
+
+            return this.RedirectToAction(nameof(Index));
+        }
+    }
+}
