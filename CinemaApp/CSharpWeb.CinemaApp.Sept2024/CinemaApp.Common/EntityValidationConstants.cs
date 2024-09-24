04517023e5ae6a074db7f32ff4cbcd1095f4d340
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2024-09-24 20:35:10 +0300

    update

--- CinemaApp/CSharpWeb.CinemaApp.Sept2024/CinemaApp.Common/EntityValidationConstants.cs
+++ CinemaApp/CSharpWeb.CinemaApp.Sept2024/CinemaApp.Common/EntityValidationConstants.cs
@@ -5,9 +5,14 @@
         public static class Movie
         {
             public const int TitleMaxLength = 50;
+            public const int GenreMinLength = 5;
             public const int GenreMaxLength = 20;
+            public const int DirectorNameMinLength = 2;
             public const int DirectorNameMaxLength = 80;
-            public const int DescriptionMaxLength = 300;
+            public const int DescriptionMinLength = 50;
+            public const int DescriptionMaxLength = 500;
+            public const int DurationMinValue = 2;
+            public const int DurationMaxValue = 999;
         }
     }
 }
--- CinemaApp/CSharpWeb.CinemaApp.Sept2024/CinemaApp.Web/Controllers/MovieController.cs
+++ CinemaApp/CSharpWeb.CinemaApp.Sept2024/CinemaApp.Web/Controllers/MovieController.cs
@@ -1,5 +1,7 @@
-﻿using CinemaApp.Data;
+﻿using System.Globalization;
+using CinemaApp.Data;
 using CinemaApp.Data.Models;
+using CinemaApp.Web.ViewModels.Movie;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CinemaApp.Web.Controllers
@@ -31,13 +33,64 @@ namespace CinemaApp.Web.Controllers
         }
 
         [HttpPost]
-        public IActionResult Create(Movie movie)
+        public IActionResult Create(AddMovieInputModel inputModel)
         {
-            //TODO: Add form model + validation
+           
+
+            bool isReleaseDateValid = DateTime.TryParseExact(inputModel.ReleaseDate, "dd/MM/yyyy",
+                CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime releaseDate);
+
+            if (!isReleaseDateValid)
+            {
+                this.ModelState.AddModelError(nameof(inputModel.ReleaseDate), "The Release Date must be in the following format : dd/MM/yyyy");
+                return this.View(inputModel);
+            }
+
+            if (!this.ModelState.IsValid)
+            {
+                // Render the same form with user entered values + model errors
+                return this.View(inputModel);
+            }
+
+            Movie movie = new Movie()
+            {
+                Title = inputModel.Title,
+                Genre = inputModel.Genre,
+                ReleaseDate = releaseDate,
+                Director = inputModel.Director,
+                Duration = inputModel.Duration,
+                Description = inputModel.Description
+            };
+
             this.dbContext.Movies.Add(movie);
             this.dbContext.SaveChanges();
 
             return this.RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        public IActionResult Details(string id)
+        { 
+            // check if id is valid Guid
+            bool isIdValid = Guid.TryParse(id, out Guid guidId);
+
+            if (!isIdValid)
+            {
+                // invalid id format
+                return this.RedirectToAction(nameof(Index));
+            }
+
+            Movie? movie = this.dbContext
+                .Movies
+                .FirstOrDefault(m => m.Id == guidId);
+
+            if (movie == null)
+            {
+                // Non-existing movie guid
+                return this.RedirectToAction(nameof(Index));
+            }
+
+            return this.View(movie);
         }
     }
 }
