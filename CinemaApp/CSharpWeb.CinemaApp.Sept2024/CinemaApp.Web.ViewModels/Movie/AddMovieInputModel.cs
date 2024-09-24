25859b569cdc264281e739f477ebd9763d3763ea
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2024-09-24 20:35:29 +0300

    up

---
+++ CinemaApp/CSharpWeb.CinemaApp.Sept2024/CinemaApp.Web.ViewModels/Movie/AddMovieInputModel.cs
@@ -0,0 +1,33 @@
+﻿namespace CinemaApp.Web.ViewModels.Movie
+{
+    using System.ComponentModel.DataAnnotations;
+    using static Common.EntityValidationConstants.Movie;
+    public class AddMovieInputModel
+    {
+        [Required]
+        [MaxLength(TitleMaxLength)]
+        public string Title { get; set; } = null!;
+
+        [Required]
+        [MinLength(GenreMinLength)]
+        [MaxLength(GenreMaxLength)]
+        public string Genre { get; set; } = null!;
+
+        [Required]
+        public string ReleaseDate { get; set; } = null!;
+
+        [Range(DurationMinValue, DurationMaxValue)]
+        public int Duration { get; set; }
+
+        [Required]
+        [MinLength(DirectorNameMinLength)]
+        [MaxLength(DirectorNameMaxLength)]
+
+        public string Director { get; set; } = null!;
+
+        [Required]
+        [MinLength(DescriptionMinLength)]
+        [MaxLength(DescriptionMaxLength)]
+        public string Description { get; set; } = null!;
+    }
+}
