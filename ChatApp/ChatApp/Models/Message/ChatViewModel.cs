e9e112659d91a07371ea53a4adbdced2a949e30a
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2024-09-25 20:10:31 +0300

    models

---
+++ ChatApp/ChatApp/Models/Message/ChatViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace ChatApp.Models.Message
+{
+    public class ChatViewModel
+    {
+        public MessageViewModel CurrentMessage { get; set; } = null!;
+        public List<MessageViewModel> Messages { get; set; } = null!;
+
+        // it isn't good practice to store the data in variable like List<>, because in every time we close the app, the massages will be deleted.
+        // we must save them in database
+    }
+}
