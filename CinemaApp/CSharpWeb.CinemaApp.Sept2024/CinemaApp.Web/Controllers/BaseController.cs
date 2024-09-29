17cdd440672262ded8736173ee9b853f6481bdcc
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2024-09-29 08:06:07 +0300

    base controller

---
+++ CinemaApp/CSharpWeb.CinemaApp.Sept2024/CinemaApp.Web/Controllers/BaseController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CinemaApp.Web.Controllers
+{
+    public class BaseController:Controller
+    {
+        protected bool IsGuidValid(string? id, ref Guid cinemaGuid)
+        {
+            // non-existing parameter in the URL
+            if (String.IsNullOrWhiteSpace(id))
+            {
+                return false;
+            }
+
+            // invalid parameter in the URL
+            bool isGuidValid = Guid.TryParse(id, out cinemaGuid);
+
+            if (!isGuidValid)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
