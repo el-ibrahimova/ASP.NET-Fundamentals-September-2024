b53e073750427c9d613f873ff775c685897f0136
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2024-09-29 07:47:51 +0300

    add view models to Movie

---
+++ CinemaApp/CSharpWeb.CinemaApp.Sept2024/CinemaApp.Web.ViewModels/Movie/AddMovieToCinemaInputModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using CinemaApp.Web.ViewModels.Cinema;
+using static CinemaApp.Common.EntityValidationConstants.Movie;
+
+namespace CinemaApp.Web.ViewModels.Movie
+{
+    public class AddMovieToCinemaInputModel
+    {
+        [Required]
+        public string Id { get; set; } = null!;
+
+        [Required]
+        [MaxLength(TitleMaxLength)]
+        public string MovieTitle { get; set; } = null!;
+
+        public IList<CinemaCheckBoxItemInputModel> Cinemas { get; set; } = new List<CinemaCheckBoxItemInputModel>();
+    }
+}
