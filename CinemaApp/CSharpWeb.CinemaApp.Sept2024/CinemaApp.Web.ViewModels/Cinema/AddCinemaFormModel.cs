394b36cc43eb2cded8215a54a23c0ccc3440def8
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2024-09-29 07:46:36 +0300

    add models to Cinema View Models

---
+++ CinemaApp/CSharpWeb.CinemaApp.Sept2024/CinemaApp.Web.ViewModels/Cinema/AddCinemaFormModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static CinemaApp.Common.EntityValidationConstants.Cinema;
+
+namespace CinemaApp.Web.ViewModels.Cinema
+{
+    public class AddCinemaFormModel
+    {
+        [Required]
+        [MinLength(NameMinLength)]
+        [MaxLength(NameMaxLength)]
+        public string Name { get; set; } = null!;
+
+        [Required]
+        [MinLength(LocationMinLength)]
+        [MaxLength(LocationMaxLength)]
+        public string Location { get; set; } = null!;
+    }
+}
---
+++ CinemaApp/CSharpWeb.CinemaApp.Sept2024/CinemaApp.Web.ViewModels/Cinema/CinemaDetailsViewModel.cs
@@ -0,0 +1,12 @@
+﻿using CinemaApp.Web.ViewModels.Movie;
+
+namespace CinemaApp.Web.ViewModels.Cinema
+{
+    public class CinemaDetailsViewModel
+    {
+        public string Name { get; set; } = null!;
+        public string Location { get; set; } = null!;
+
+        public IEnumerable<CinemaMovieViewModel> Movies = new HashSet<CinemaMovieViewModel>();
+    }
+}
