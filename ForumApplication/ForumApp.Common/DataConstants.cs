2da999326d97ad86f7b3c78f87b6347588fb72e5
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2024-09-30 22:11:56 +0300

    common project

---
+++ ForumApplication/ForumApp.Common/DataConstants.cs
@@ -0,0 +1,10 @@
+﻿namespace ForumApp.Common
+{
+    public static class DataConstants
+    {
+        public const int TitleMinLength = 10;
+        public const int TitleMaxLength = 50;
+        public const int ContentMinLength = 30;
+        public const int ContentMaxLength = 1500;
+    }
+}
