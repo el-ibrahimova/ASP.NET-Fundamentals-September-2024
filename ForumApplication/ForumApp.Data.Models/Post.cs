dccae61b8b2f5ad9f3d07ba3482befb6b9cca87c
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2024-09-30 22:14:47 +0300

    data project

---
+++ ForumApplication/ForumApp.Data.Models/Post.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static ForumApp.Common.DataConstants;
+
+namespace ForumApp.Data.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MinLength(TitleMaxLength)]
+        
+        public string Title { get; set; } = null!;
+
+        [Required]
+        [MaxLength(ContentMaxLength)]
+        public string Content { get; set; } = null!;
+    }
+}
