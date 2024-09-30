f89621de5ba999f55f0ede41fa9c10b26e135fff
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2024-09-30 22:12:57 +0300

    migrations

---
+++ ForumApplication/ForumApp.Data/Migrations/20240929182315_seedPosts.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace ForumApp.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class seedPosts : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Posts",
+                columns: new[] { "Id", "Content", "Title" },
+                values: new object[,]
+                {
+                    { 1, "My first post will be about performing CRUD operations in MVC", "My first post" },
+                    { 2, "My secon post will be about performing CRUD operations in MVC", "My second post" },
+                    { 3, "My third post will be about performing CRUD operations in MVC", "My third post" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Posts",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Posts",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Posts",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
