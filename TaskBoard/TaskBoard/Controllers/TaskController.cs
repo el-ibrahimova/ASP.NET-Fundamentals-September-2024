512e2017540437a2d500f37af787d20e8bb8ec17
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2024-10-05 21:34:47 +0300

    create TaskController and view for Create New Task

---
+++ TaskBoard/TaskBoard/Controllers/TaskController.cs
@@ -0,0 +1,76 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TaskBoard.Data;
+using TaskBoard.Models;
+
+namespace TaskBoard.Controllers
+{
+    [Authorize]
+    public class TaskController : Controller
+    {
+
+        private readonly TaskBoardAppDbContext data;
+
+        public TaskController(TaskBoardAppDbContext context)
+        {
+            data = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Create()
+        {
+            var model = new TaskFormViewModel();
+
+            model.Boards = await GetBoards();
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(TaskFormViewModel model)
+        {
+            if (!(await GetBoards()).Any(b => b.Id == model.BoardId))
+            {
+                ModelState.AddModelError(nameof(model.BoardId), "Board does not exist");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                model.Boards = await GetBoards();
+                return View(model);
+            }
+
+            var entity = new TaskBoard.Data.Task()
+            {
+                BoardId = model.BoardId,
+                CreatedOn = DateTime.Now,
+                Description = model.Description,
+                OwnerId = GetUserId(),
+                Title = model.Title
+            };
+
+            await data.AddAsync(entity);
+           await data.SaveChangesAsync();
+
+            return RedirectToAction("Index", "Board");
+        }
+
+        private string GetUserId()
+        {
+            return User.FindFirstValue(ClaimTypes.NameIdentifier);
+        }
+
+
+        private async Task< IEnumerable<TaskBoardModel>> GetBoards()
+        {
+            return await data.Boards
+                 .Select(x => new TaskBoardModel
+                 {
+                     Id = x.Id,
+                     Name = x.Name
+                 })
+                 .ToListAsync();
+        }
+    }
+}
---
+++ TaskBoard/TaskBoard/Models/TaskFormViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static TaskBoard.Data.DataConstants;
+
+namespace TaskBoard.Models
+{
+    public class TaskFormViewModel
+    { public int Id { get; set; }
+
+        [Required(ErrorMessage = ErrorMessages.RequireError)]
+        [StringLength(TaskTitleMaxLength, 
+            MinimumLength = TaskTitleMinLength, 
+            ErrorMessage = ErrorMessages.StringLengthError)]
+        public string Title { get; set; } = null!;
+
+        [Required(ErrorMessage = ErrorMessages.RequireError)]
+        [StringLength(TaskDescriptionMaxLength,
+            MinimumLength = TaskDescriptionMinLength,
+            ErrorMessage = ErrorMessages.StringLengthError)]
+        public string Description { get; set; } = null!;
+        
+        public int? BoardId { get; set; }
+
+        public IEnumerable<TaskBoardModel> Boards { get; set; } = new List<TaskBoardModel>();
+
+    }
+}
