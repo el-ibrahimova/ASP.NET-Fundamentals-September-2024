3225a1d4ccd2ffffa00aee3e64cc1d75f0168263
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2024-10-07 22:43:19 +0300

    create Event controller and write logic for method All

---
+++ ExamPreparation/HomiesApp/Homies/Controllers/EventController.cs
@@ -0,0 +1,32 @@
+﻿using Homies.Data;
+using Homies.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Homies.Controllers
+{
+    public class EventController : Controller
+    {
+        private readonly HomiesDbContext data;
+
+        public EventController(HomiesDbContext context)
+        {
+            data = context;
+        }
+
+        public async Task <IActionResult> All()
+        {
+            var events = await data.Events
+                .Select(e => new EventInfoViewModel(
+                    e.Id,
+                    e.Name,
+                    e.Start,
+                    e.Type.Name,
+                    e.Organiser.UserName
+                    ))
+                .ToListAsync();
+
+            return View(events);
+        }
+    }
+}
