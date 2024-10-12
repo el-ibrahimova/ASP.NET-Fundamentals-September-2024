edfa0a6d6858d180b8781bf6fbc5570c8df05bda
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2024-10-12 16:00:06 +0300

    create Genre table

--- ExamPreparation/GameZone/GameZone/Data/Models/Genre.cs
+++ ExamPreparation/GameZone/GameZone/Data/Models/Genre.cs
@@ -1,6 +1,19 @@
-﻿namespace GameZone.Data.Models
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace GameZone.Data.Models
 {
     public class Genre
     {
+        [Key]
+        [Comment("Genre Identifier")]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(25)]
+        [Comment("Ganre name")]
+        public string Name { get; set; } = null!;
+
+        public ICollection<Game> Games { get; set; } = new List<Game>();
     }
 }
