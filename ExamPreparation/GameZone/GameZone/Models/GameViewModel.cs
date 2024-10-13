7aecee04f878df08b5fae4124b50b0889509b1f4
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2024-10-13 07:38:03 +0300

    Create GameViewModel.cs

---
+++ ExamPreparation/GameZone/GameZone/Models/GameViewModel.cs
@@ -0,0 +1,28 @@
+﻿using GameZone.Data.Models;
+using System.ComponentModel.DataAnnotations;
+using static GameZone.Common.ModelConstants;
+
+namespace GameZone.Models
+{
+    public class GameViewModel
+    {
+
+        [Required]
+        [StringLength(GameTitleMaxLength, MinimumLength = GameTitleMinLength)]
+        public string Title { get; set; } = string.Empty;
+
+        public string? ImageUrl { get; set; }
+
+        [Required]
+        [StringLength(GameDescriptionMaxLength, MinimumLength = GameDescriptionMinLength)]
+        public string Description { get; set; } = string.Empty;
+
+        [Required]
+        public string ReleasedOn { get; set; } = DateTime.Today.ToString(GameReleasedOnDateFormat);
+
+        [Required]
+        public int GenreId { get; set; }
+
+        public List<Genre> Genres { get; set; } = new List<Genre>();
+        }
+}
