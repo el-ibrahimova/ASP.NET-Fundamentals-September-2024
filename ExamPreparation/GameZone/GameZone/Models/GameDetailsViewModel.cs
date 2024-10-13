ecf061f3201955691cfc4089c523c9024185bd11
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2024-10-13 07:38:16 +0300

    Create GameDetailsViewModel.cs

---
+++ ExamPreparation/GameZone/GameZone/Models/GameDetailsViewModel.cs
@@ -0,0 +1,25 @@
+﻿using GameZone.Data.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace GameZone.Models
+{
+    public class GameDetailsViewModel
+    {
+        public int Id { get; set; }
+        public required string Title { get; set; } 
+
+        public required string Description { get; set; } 
+        
+        public string? ImageUrl { get; set; }
+
+        public required string ReleasedOn { get; set; }
+
+        public required string Genre { get; set; } 
+
+        public required string Publisher { get; set; }
+    }
+}
+
