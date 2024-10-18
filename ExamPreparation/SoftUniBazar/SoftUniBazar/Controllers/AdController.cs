728b304b47455599876c4106ca11844f12700201
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2024-10-18 10:43:33 +0300

    change user's access to login and register, change access to home page

--- ExamPreparation/SoftUniBazar/SoftUniBazar/Controllers/AdController.cs
+++ ExamPreparation/SoftUniBazar/SoftUniBazar/Controllers/AdController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using SoftUniBazar.Data;
+using SoftUniBazar.Models;
+using static SoftUniBazar.Common.EntityConstants;
 
 namespace SoftUniBazar.Controllers
 {
@@ -15,7 +18,32 @@ namespace SoftUniBazar.Controllers
         [HttpGet]
         public async Task<IActionResult> All()
         {
-            return View();
+            var model = await data.Ads
+                .Select(a => new AllAdViewModel()
+                {
+                    Category = a.Category.Name,
+                    CreatedOn = a.CreatedOn.ToString(EntityDateFormat),
+                    Description = a.Description,
+                    Id = a.Id,
+                    ImageUrl = a.ImageUrl,
+                    Name = a.Name,
+                    Owner = a.Owner.UserName,
+                    Price = a.Price
+                })
+                .ToListAsync();
+
+            return View(model);
+        }
+
+        private async Task<IEnumerable<CategoryViewModel>> GetCategories()
+        {
+            return await data.Categories
+                .Select(c => new CategoryViewModel()
+                {
+                    Id = c.Id,
+                    Name = c.Name
+                })
+                .ToListAsync();
         }
     }
 }
