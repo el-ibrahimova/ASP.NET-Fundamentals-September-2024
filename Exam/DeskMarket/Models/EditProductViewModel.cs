92ac40423986041c12ceb9afacd4645cdba9d093
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2024-10-19 20:05:46 +0300

    add models

---
+++ Exam/DeskMarket/Models/EditProductViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+using static DeskMarket.Common.EntityConstants;
+
+namespace DeskMarket.Models
+{
+    public class EditProductViewModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(ProductNameMaxLength, MinimumLength = ProductNameMinLength)]
+        public string ProductName { get; set; }=null!;
+
+        [Required]
+        [Range(ProductPriceMinValue, ProductPriceMaxValue)]
+        public decimal Price { get; set; }
+
+        [Required]
+        [StringLength(ProductDescriptionMaxLength, MinimumLength = ProductDescriptionMinLength)]
+        public string Description { get; set; } = null!;
+
+        public string? ImageUrl { get; set; }
+        public string AddedOn { get; set; } = null!;
+
+        public int CategoryId { get; set; }
+        public IEnumerable<CategoryViewModel> Categories { get; set; } = new HashSet<CategoryViewModel>();
+
+        [Required]
+        public string SellerId { get; set; } = null!;
+    }
+}
---
+++ Exam/DeskMarket/Models/IndexViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace DeskMarket.Models
+{
+    public class IndexViewModel
+    {
+        public string ProductName { get; set; } = null!;
+        public decimal Price { get; set; }
+        public string? ImageUrl { get; set; }
+        public int Id { get; set; }
+        public bool IsSeller { get; set; }
+        public bool HasBought { get; set; }
+    }
+}
