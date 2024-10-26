f4aaed6833b84a24415c1b0ab5943034dd405a97
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2024-10-26 10:54:36 +0300

    write logic in methods from Repository

--- CinemaApp/CSharpWeb.CinemaApp.Sept2024/CinemaApp.Data/Repository/Interfaces/IRepository.cs
+++ CinemaApp/CSharpWeb.CinemaApp.Sept2024/CinemaApp.Data/Repository/Interfaces/IRepository.cs
@@ -3,6 +3,21 @@
     public interface IRepository<TType, TId>
     {
         TType GetById(TId id);
-        TType GetByIdAsync(TId id);
+        Task<TType> GetByIdAsync(TId id);
+
+        IEnumerable<TType> GetAll();
+        Task<IEnumerable<TType>> GetAllAsync();
+
+        // we stay attached to the base
+        IEnumerable<TType> GetAllAttached();
+
+        void Add(TType item);
+        Task AddAsync(TType item);
+
+        bool Delete(TId id);
+        Task<bool> DeleteAsync(TId id);
+
+        bool Update(TType item);
+        Task<bool> UpdateAsync(TType item);
     }
 }
--- CinemaApp/CSharpWeb.CinemaApp.Sept2024/CinemaApp.Data/Repository/Repository.cs
+++ CinemaApp/CSharpWeb.CinemaApp.Sept2024/CinemaApp.Data/Repository/Repository.cs
@@ -1,12 +1,125 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CinemaApp.Data.Repository.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 namespace CinemaApp.Data.Repository
 {
-    internal class Repository
+    public class Repository<TType, TId> : IRepository<TType, TId>
+    where TType : class
     {
+        private readonly CinemaDbContext dBcontext;
+        private readonly DbSet<TType> dbSet;
+
+        public Repository(CinemaDbContext dbContext)
+        {
+            this.dBcontext = dbContext;
+            this.dbSet = this.dBcontext.Set<TType>();
+        }
+
+        // we set nullable to disabled => from CinemaApp.Data=>Properties
+        public TType GetById(TId id)
+        {
+            TType entity = this.dbSet
+                .Find(id);
+
+            return entity;
+        }
+
+        public async Task<TType> GetByIdAsync(TId id)
+        {
+            TType entity = await this.dbSet
+                .FindAsync(id);
+
+            return entity;
+        }
+
+        // we disconnect from the base with .ToArray
+        public IEnumerable<TType> GetAll()
+        {
+            return this.dbSet.ToArray();
+        }
+
+        public async Task<IEnumerable<TType>> GetAllAsync()
+        {
+            return await this.dbSet.ToArrayAsync();
+        }
+
+        // we stay attached to the base
+        public IEnumerable<TType> GetAllAttached()
+        {
+            return this.dbSet.AsQueryable();
+        }
+
+
+        public void Add(TType item)
+        {
+            this.dbSet.Add(item);
+            this.dBcontext.SaveChanges();
+        }
+
+        public async Task AddAsync(TType item)
+        {
+            await this.dbSet.AddAsync(item);
+            await this.dBcontext.SaveChangesAsync();
+        }
+
+        public bool Delete(TId id)
+        {
+            TType entity = this.GetById(id);
+
+            if (entity == null)
+            {
+                return false;
+            }
+
+            this.dbSet.Remove(entity);
+            this.dBcontext.SaveChanges();
+            return true;
+        }
+
+        public async Task<bool> DeleteAsync(TId id)
+        {
+            TType entity = await this.GetByIdAsync(id);
+
+            if (entity == null)
+            {
+                return false;
+            }
+
+            this.dbSet.Remove(entity);
+            await this.dBcontext.SaveChangesAsync();
+            return true;
+        }
+
+        public bool Update(TType item)
+        {
+            try
+            {
+                this.dbSet.Attach(item);
+                this.dBcontext.Entry(item).State = EntityState.Modified;
+                this.dBcontext.SaveChanges();
+
+                return true;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+        }
+
+        public async Task<bool> UpdateAsync(TType item)
+        {
+            try
+            {
+                this.dbSet.Attach(item);
+                this.dBcontext.Entry(item).State = EntityState.Modified;
+                await this.dBcontext.SaveChangesAsync();
+
+                return true;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+        }
     }
 }
