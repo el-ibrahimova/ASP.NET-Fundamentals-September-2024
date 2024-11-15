da5a1e54df5f93b61817869418b477332fc67e97
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2024-11-15 22:34:45 +0200

    create ITicketService and TicketService

---
+++ CinemaApp/CSharpWeb.CinemaApp.Sept2024/CinemaApp.Services.Data/TicketService.cs
@@ -0,0 +1,27 @@
+﻿using CinemaApp.Common;
+using CinemaApp.Data.Models;
+using CinemaApp.Data.Repository.Interfaces;
+using CinemaApp.Services.Data.Interfaces;
+using CinemaApp.Services.Mapping;
+using CinemaApp.Web.ViewModels.CinemaMovie;
+
+namespace CinemaApp.Services.Data
+{
+    
+    public class TicketService:BaseService, ITicketService
+    {
+        private readonly IRepository<CinemaMovie, object> cinemaMovieRepository;
+
+        public TicketService(IRepository<CinemaMovie, object> cinemaMovieRepository)
+        {
+            this.cinemaMovieRepository = this.cinemaMovieRepository;
+        }
+        public Task<bool> SetAvailableTicketsAsync(SetAvailableTicketsViewModel model)
+        {
+            CinemaMovie cinemaMovie = AutoMapperConfig.MapperInstance.Map<CinemaMovie>(model);
+
+         
+            this.cinemaMovieRepository.UpdateAsync(cinemaMovie);
+        }
+    }
+}
